5837cc4347bfc5ba0a2f129fc7bf3aece64ae745
ericcapiz <ericcapiz@gmail.com>
2021-03-12 14:32:56 -0600

    MVP

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
+const postRouter = require('./posts/posts-router');
+
+server.use(express.json());
+server.use('/api/posts', postRouter)
 // require your posts router and connect it here
+
+server.get('/', (req,res)=>{
+    res.send(`<h2>Router API</h2>`)
+})
+
+module.exports = server;
